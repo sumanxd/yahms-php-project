99f458d960a2b26b0a3353eabd3d3c4a913389fa
Suman Das <40028942+sumanxd@users.noreply.github.com>
2018-06-30 13:49:13 +0530

    Add files via upload

---
+++ WS/sunandini/indent.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sunandini Indent</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="../../style.css">
+	<link rel="stylesheet" type="text/css" href="../../form.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <style>
+
+</style>
+</head>
+<body>
+	<section class="container-fluid">
+		<div class="container-fluid">
+			<div class="row">
+	
+				<a class="toggleMenu" href="#">Menu</a>
+				<center>
+					<ul class="nav">
+						<li><a href="../index.html">HOME</a></li>
+						<li  class="test">
+							<a href="../../projectnew.php">District Profile</a>
+								<ul>
+									<li>
+										<a href="#">About Us</a>
+										<!-- <ul>
+											<li><a href="cattle.php">Cattle</a></li>
+											<li><a href="buffalo.php">Buffalo</a></li>
+											<li><a href="sheep.php">Sheep & Goat</a></li>
+											<li><a href="poultry.php">Poultry</a></li>
+											<li><a href="dogs.php">Dogs</a></li>
+											<li><a href="cats.php">Cats</a></li>
+											<li><a href="horses.php">Horses</a></li>
+											<li><a href="#">Others</a></li>
+										</ul> -->
+									</li>
+									<li><a href="../profile/deworming.php">Objectives</a></li>
+									<li><a href="../profile/castrations.php">Staff Position</a></li>
+									<li><a href="../profile/vaccinations.php">No Of Veterinary Institutes</a></li>
+									<li><a href="../profile/ai.php">Performance Of The District</a></li>
+									<li><a href="../profile/fodder.php">Livestock Population</a></li>
+									<li><a href="../profile/calf.php">Core Indicators</a></li>
+									<li><a href="../profile/fertilitycamps.php">Schemes</a></li>
+									<li><a href="../profile/farmerawarness.php">Any Others</a></li>
+									
+									
+								</ul>
+						</li>
+						<!-- <li><a href="#">Monthly Progressive Reports</a></li> -->
+						
+						<li><a href="#"></a></li>
+						<!-- <li><a href="#">Administrative Works</a></li> -->
+						<li><a href="#">Functional Indicators</a>
+							<ul>
+								<li><a href="#">Curative Treatment</a>
+									<ul>
+										<li><a href="../../FI/cattle.php">Cattle</a></li>
+										<li><a href="../../FI/buffalo.php">Buffalo</a></li>
+										<li><a href="../../FI/sheepgoat.php">Sheep & Goat</a></li>
+										<li><a href="../../FI/poultry.php">Poultry</a></li><!--/cats.php/-->
+										<li><a href="../../FI/dogs.php">Dogs</a></li>
+										<li><a href="../../FI/cats.php">Cats</a></li>
+										<li><a href="../../FI/horses.php">Horses</a></li>
+										<li><a href="#">Others</a></li>
+									</ul>
+								</li>
+								<li><a href="../../FI/deworming.php">Preventive Deworming</a> </li>
+								<li><a href="../../FI/castrations.php">Castrations</a> </li>
+								<li><a href="../../FI/vaccinations.php">Vaccinations</a> </li>
+								<li><a href="../../FI/ai.php">Artificial Insemination(AI)</a> </li>
+								<li><a href="../../FI/fodderdevlopment.php">Fodder Devlopment</a> </li>
+								<li><a href="../../FI/calf.php">Calf Births</a> </li>
+								<li><a href="../../FI/fertilitycamps.php">Fertility Camps</a> </li>
+								<li><a href="../../FI/farmerawarness.php">Farmer Awarness</a> </li>
+								<li><a href="../../FI/extensionprograms.php">Extension Programs</a> </li>
+								
+							</ul>
+						</li>
+						<li><a href="../../coreindicators.php">Core Indicators</a>
+							
+						</li>
+						<li><a href="#">Welfare Schemes</a>
+							<ul>
+								<li><a href="#">Sheep</a>
+									<ul>
+										<li><a href="../postgrounding.php">Post Grounding</a> </li>
+										<li><a href="../grounding.php">Grounding</a></li>
+										<li> <a href="../pregrounding.php">Pre Grounding</a></li>
+										
+									</ul>
+								</li>
+								<li><a href="#">Fodder Seed</a>
+									<ul>
+										<li><a href="../fodder/indent.php">Indent From The Field</a> </li>
+										<li><a href="../fodder/payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Chaff Cutter</a>
+									<ul>
+										<li><a href="../chaff/indent.php">Indent From The Field</a> </li>
+										<li><a href="../chaff/payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Back Yard Poultry</a>
+									<ul>
+										<li><a href="../back/indent.php">Indent From The Field</a> </li>
+										<li><a href="../back/payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Sunandini calf-Milk Program</a>
+									<ul>
+										<li><a href="indent.php">Indent From The Field</a> </li>
+										<li><a href="payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Miltch Animals</a>
+									<ul>
+										<li><a href="../miltch/indent.php">Indent From The Field</a> </li>
+										<li><a href="../miltch/payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Piggeries</a>
+									<ul>
+										<li><a href="../pig/indent.php">Indent From The Field</a> </li>
+										<li><a href="../pig/payment.php">Payment Particulars</a></li>
+									</ul>
+								</li>
+								
+							</ul>
+						</li>
+						<li><a href="../../suggestion.php">Suggestion(Collecter)</a></li>
+						<li><a href="../../asuggestion.php">Suggestion(Admin)</a></li>
+						<li><a href="#">Success Stories</a></li>
+						
+						<li><a href="#">Log In</a>
+							<ul>
+								<li><a href="../../collecter/login.php">Collector</a></li>
+								<li><a href="../../admin/login.php">Admin</a></li>
+								<li><a href="../../mandaladmin/login.php">Mandal Admin</a></li>
+								<li><a href="#">Ad/AH</a></li>
+							</ul>
+						</li>
+						
+						
+					</ul>
+				</center>
+			</div>
+		</div>
+		
+	</section>
+	<section class="space">
+		<div class="container">
+			<div class="row ">
+			<center><h2>Sunandini Indent</h2></center>
+				<form action="uploadnew26.php" method="post" enctype="multipart/form-data" onsubmit="return validateForm()"  id="myfrm1">
+					<div class="col-md-4">
+						
+						
+						
+						
+  <label for="yearly">Type</label>
+ 	<select class="form-control input-lg" id="chaff" name="chaff">
+				  <option>--Type Of Calf Program--</option>
+				  <option name="typechaff">HF Cross</option>
+				  <option name="typechaff">Jersey</option>	  
+				  <option name="typechaff">Murra</option>
+				  <option name="typechaff">Ongole</option>
+				  <option name="typechaff">Tarparker</option>
+				</select>
+						
+						
+						<div class="form-group">
+							  <label for="yearly">Full Cost</label>
+							  <input class="form-control input-lg" id="yearly" name="cost" type="text">
+						</div>
+						<div class="form-group">
+							  <label for="yearly">Subsidy</label>
+							  <input class="form-control input-lg" id="yearly" name="subs" type="text">
+						</div>
+						
+						
+					</div>
+					<div class="col-md-4">
+					<div class="form-group">
+							  <label for="yearly">Beneficial Contribution</label>
+							  <input class="form-control input-lg" id="yearly" name="beni" type="text">
+						</div>
+						
+						<div class="form-group">
+							<label for="mandal">Mandal Name</label>
+								<select class="form-control input-lg" id="mandal" name="mandal">
+									<option value="item0">--Select Mandal--</option>
+									<option value="item1">Addagudur</option>
+									<option value="item2">Alair</option>
+									<option value="item3">Athmakur</option>
+									<option value="item4">Bhongiri</option>
+									<option value="item5">BibiNagar</option>
+									<option value="item6">Bommalaramaram</option>
+									<option value="item7">Choutuppal</option>
+									<option value="item8">Motakondur</option>
+									<option value="item9">Mothkur</option>
+									<option value="item10">Narayanpur</option>
+									<option value="item11">Pochampally</option>
+									<option value="item12">Rajapet</option>
+									<option value="item13">Ramannapet</option>
+									<option value="item14">Thurkapally</option>
+									<option value="item15">Valigonda</option>
+									<option value="item16">Yadagirigutta</option>
+								</select>
+						</div>
+						<div class="form-group">
+							<label for="village">Village Name</label>
+							<select class="form-control input-lg" id="village" type="text" name="village" >
+								<option value="">-- Select Your Village -- </option>
+							</select>
+						</div>
+					</div>
+					
+					<div class="col-md-4">
+						
+						<div class="form-group">
+							  <label for="treat">Indent Of Quality By Doctors</label>
+							  <input class="form-control input-lg" id="treat" type="text" name="iq" >
+						</div>
+						</div>
+						
+						
+					<div class="col-md-6">
+						<label class="btn btn-primary ">
+							<input type="file" name="file" hidden>
+						</label>
+					</div>
+					<div class="col-md-4">
+						<button class="btn btn-warning btn-lg" type="submit" name="btn-upload">Submit</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</section>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="../../script.js"></script>
+<script>
+$(document).ready(function () {
+    $("#mandal").change(function () {
+        var val = $(this).val();
+        if (val == "item1") {
+        $("#village").html
+("<option>Addagudur</option><option>Chinnapadishala</option><option>kanchanpally</option>");
+        } 
+		else if (val == "item2") {
+            $("#village").html
+("<option>Alair</option><option>Kolanpaka</option><option>Sharajpet</option>");
+        } else if (val == "item3") {
+            $("#village").html
+("<option>Atamakur</option><option>Parupally</option><option>Kullera</option><option>Kapraipally</option><option> Pallerla</option>");
+        } else if (val == "item4") {
+            $("#village").html
+("<option>Bhongir</option><option>Raigiri</option><option>Pagidipally</option><option>Chandupatla</option><option>Wadaparthy</option><option> Bollepally</option><option> Anajipoor</option><option> NR.Pally</option>");
+        }
+		else if (val == "item5") {
+            $("#village").html
+("<option>Bibinagar</option><option>Chinnaravulapally</option><option>Kondamadugu</option><option>Brahmanpally</option><option>Venkiryal</option>");
+        }
+		else if (val == "item6") {
+            $("#village").html
+("<option>B.Ramaram</option><option>Mariyala</option><option>Medipally</option><option>Jalapur</option>");
+        }
+		else if (val == "item7") {
+            $("#village").html
+("<option>Chotuppal</option><option>D.Nagaram</option><option>Malkhapur</option><option>Peddakondur</option>");
+        }
+		else if (val == "item8") {
+            $("#village").html
+("<option>Mothkur</option><option>Dattappaguda</option>");
+        }
+		else if (val == "item9") {
+            $("#village").html
+("<option>Motakondur</option><option>Chada</option><option>Chandepally</option>");
+        }
+		else if (val == "item10") {
+            $("#village").html
+("<option>Narayanpur</option><option>Survail</option>");
+        }
+		else if (val == "item11") {
+            $("#village").html
+("<option>Pochampally</option><option>Jiblakpally</option><option>Indriyal</option><option>Juloor</option>");
+        }
+		else if (val == "item12") {
+            $("#village").html
+("<option>Rajapet</option><option>Begumpet</option><option>Bondugula</option><option>Raghunathpur</option><option>Challur</option><option> Bollepally</option><option> Anajipoor</option><option> NR.Pally</option>");
+        }
+		else if (val == "item13") {
+            $("#village").html
+("<option>Ramannapet</option><option>Tummalaguda</option><option>Yellanki</option><option>Kakkireni</option><option>Munipampula</option>");
+        }
+		else if (val == "item14") {
+            $("#village").html
+("<option>Thurkapally</option><option>Veerareddy pally</option><option>Madhapur</option><option>Dattaipally</option>");
+        }
+		else if (val == "item15") {
+            $("#village").html
+("<option>Valigonda</option><option>Arror</option><option>Gokaram</option><option>Vemulakonda</option><option>Thurkapally-valigonda</option><option> Puligilla</option><option>Pahilwanpur</option><option>Sunkishala</option><option>T.Somaram</option>");
+        }else if (val == "item16") {
+            $("#village").html
+("<option>Yadagirigutta</option><option>Kacharam</option><option>Mallapur</option><option>Wangapally</option><option>Chinnakandukur</option><option> Chollair</option>");
+        }
+    });
+});
+</script>
+</body>
+</html>
