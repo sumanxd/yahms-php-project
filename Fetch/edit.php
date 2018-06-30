5b93e7eb6c6fefaaa3b38ae89f4d2161977fe767
Suman Das <40028942+sumanxd@users.noreply.github.com>
2018-06-30 12:53:59 +0530

    Add files via upload

---
+++ Fetch/edit.php
@@ -0,0 +1,237 @@
+<?php
+require('../dbno.php');
+
+$sid=$_REQUEST['sid'];
+$query = "SELECT * from staff where sid='".$sid."'"; 
+$result = mysqli_query($conn, $query) or die ( mysqli_error());
+$row = mysqli_fetch_assoc($result);
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Update Record</title>
+<link rel="stylesheet" />
+<style>
+
+#playground-container {
+    height: 500px;
+    overflow: hidden !important;
+    -webkit-overflow-scrolling: touch;
+}
+body, html{
+     height: 100%;
+ 	background-repeat: no-repeat;
+
+ 	font-family: 'Oxygen', sans-serif;
+	    background-size: cover;
+}
+
+.main{
+ 	margin:50px 15px;
+}
+
+h1.title { 
+	font-size: 50px;
+	font-family: 'Passion One', cursive; 
+	font-weight: 400; 
+}
+
+hr{
+	width: 10%;
+	color: #fff;
+}
+
+.form-group{
+	margin-bottom: 15px;
+}
+
+label{
+	margin-bottom: 15px;
+}
+
+input,
+input::-webkit-input-placeholder {
+    font-size: 11px;
+    padding-top: 3px;
+}
+
+.main-login{
+ 	background-color: #fff;
+    /* shadows and rounded borders */
+    -moz-border-radius: 2px;
+    -webkit-border-radius: 2px;
+    border-radius: 2px;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+
+}
+.form-control {
+    height: auto!important;
+padding: 8px 12px !important;
+}
+.input-group {
+    -webkit-box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.21)!important;
+    -moz-box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.21)!important;
+    box-shadow: 0px 2px 5px 0px rgba(0,0,0,0.21)!important;
+}
+#button {
+    border: 1px solid #ccc;
+    margin-top: 28px;
+    padding: 6px 12px;
+    color: #666;
+    text-shadow: 0 1px #fff;
+    cursor: pointer;
+    -moz-border-radius: 3px 3px;
+    -webkit-border-radius: 3px 3px;
+    border-radius: 3px 3px;
+    -moz-box-shadow: 0 1px #fff inset, 0 1px #ddd;
+    -webkit-box-shadow: 0 1px #fff inset, 0 1px #ddd;
+    box-shadow: 0 1px #fff inset, 0 1px #ddd;
+    background: #f5f5f5;
+    background: -moz-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #f5f5f5), color-stop(100%, #eeeeee));
+    background: -webkit-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -o-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: -ms-linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    background: linear-gradient(top, #f5f5f5 0%, #eeeeee 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f5f5f5', endColorstr='#eeeeee', GradientType=0);
+}
+.main-center{
+ 	margin-top: 30px;
+ 	margin: 0 auto;
+ 	max-width: 400px;
+    padding: 10px 40px;
+	background:#009edf;
+	    color: #FFF;
+    text-shadow: none;
+	-webkit-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+-moz-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+
+}
+span.input-group-addon i {
+    color: #009edf;
+    font-size: 17px;
+}
+
+.login-button{
+	margin-top: 5px;
+}
+
+.login-register{
+	font-size: 11px;
+	text-align: center;
+}
+
+</style>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<!-- Website Font style -->
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+		<!-- Google Fonts -->
+	
+</head>
+<body>
+<div class="form">
+
+
+<?php
+$status = "";
+if(isset($_POST['new']) && $_POST['new']==1)
+{
+$sid=$_REQUEST['sid'];
+
+$name =$_REQUEST['name'];
+$post_sanctioned =$_REQUEST['post_sanctioned'];
+$post_filled =$_REQUEST['post_filled'];
+$no_vacant =$_REQUEST['no_vacant'];
+
+$update="update staff set 
+name='".$name."', post_sanctioned='".$post_sanctioned."', post_filled='".$post_filled."', no_vacant='".$no_vacant."' where sid='".$sid."'";
+mysqli_query($conn, $update) or die( mysqli_connect_error());
+echo "<script type='text/javascript'> alert('Successfully Updated');</script>";
+echo "<script>location.href='../projectnew.php'</script>";
+}else {
+?>
+<div>
+
+
+
+<div class="container">
+			<div class="row main">
+				<div class="main-login main-center">
+				<h2>Update Record</h2>
+<form name="form" method="post" action=""> 
+						<input type="hidden" name="new" value="1" />
+<input name="id" type="hidden" value="<?php echo $row['id'];?>" />
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">Name</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="name"   placeholder="Enter your Name" required value="<?php echo $row['name'];?>"/>
+								</div>
+							</div>
+						</div>
+
+						
+
+						
+
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">Post Sunctioned</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="post_sanctioned"   placeholder="vehicle no" required value="<?php echo $row['post_sanctioned'];?>"/>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">Post Filled</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="post_filled"   placeholder="rate" required value="<?php echo $row['post_filled'];?>"/>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="name" class="cols-sm-2 control-label">NNo vacant</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+									<input type="text" class="form-control" name="no_vacant"   placeholder="rate" required value="<?php echo $row['no_vacant'];?>"/>
+								</div>
+							</div>
+						</div>
+						
+						
+						
+
+						<div class="form-group ">
+							<p><input name="submit" type="submit" value="Update" style="    color: white;
+    background-color: red;
+    padding: 7px 51px;
+    margin-left: 85px;"/></p>
+						</div>
+				
+					</form>
+					</div>
+			</div>
+		</div>
+
+
+
+<?php } ?>
+</div>
+</div>
+</body>
+</html>
+
+
